d23e55f7d48988c3a635b51bc54bd0e90f60700e
Sadman Arnob <sadmanarnob31@gmail.com>
2023-05-16 02:21:58 +0600

    added error masseges

--- index.php
+++ index.php
@@ -41,9 +41,7 @@ if (isset($_POST['submit'])) {
         $address = test_input($_POST['address']);
 
 
-        if (empty($_POST['upfile'])) {
-            $imageError = "*Photo is required";
-        }
+        
         if (empty($_POST["name"])) {
             $nameError = "*Name is required";
         }
@@ -124,7 +122,7 @@ function test_input($data)
         </div>
 
 
-        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
 
 
 
@@ -133,31 +131,41 @@ function test_input($data)
 
 
 
+
+
+
+
+
                     <label for="image" class="form-label mt-3">Upload Image:</label>
-                    <input type="file" name="upfile" class="form-control shadow">
+                    <input type="file" name="upfile" class="form-control shadow" required>
+                    <small class="invalid-feedback">Please upload image</small>
                     <p class="text-danger"><?php echo $imageError ?></p>
 
 
 
                     <label for="email" class="form-label">Email address:</label>
-                    <div class="input-group shadow">
-                        <span class="input-group-text">
+                    <div class="input-group">
+                        <span class="input-group-text shadow">
                             <i class="bi bi-envelope-fill"></i>
                         </span>
-                        <input type="email" name="email" class="form-control">
+                        <input type="email" name="email" class="form-control shadow" required>
+                        <small class="invalid-feedback">Please enter your email</small>
                     </div>
+                    
+                    
+                   
 
                     <p class="text-danger"><?php echo $emailError ?></p>
 
 
 
-
                     <label for="name" class="form-label">Name:</label>
-                    <div class="input-group shadow">
-                        <span class="input-group-text">
+                    <div class="input-group ">
+                        <span class="input-group-text shadow">
                             <i class="bi bi-person-fill"></i>
                         </span>
-                        <input type="text" name="name" class="form-control"><br>
+                        <input type="text" name="name" class="form-control shadow" required>
+                        <small class="invalid-feedback">Please enter your name</small>
                     </div>
                     <p class="text-danger"><?php echo $nameError ?></p>
 
@@ -165,11 +173,12 @@ function test_input($data)
 
 
                     <label for="phone" class="form-label">Phone:</label>
-                    <div class="input-group shadow">
-                        <span class="input-group-text">
+                    <div class="input-group">
+                        <span class="input-group-text shadow">
                             <i class="bi bi-telephone-fill"></i>
                         </span>
-                        <input type="text" name="phone" class="form-control">
+                        <input type="text" name="phone" class="form-control shadow" required>
+                        <small class="invalid-feedback">Please enter your phone</small>
                     </div>
                     <p class="text-danger"><?php echo $phoneError ?></p>
 
@@ -191,9 +200,13 @@ function test_input($data)
 
 
 
+
+
+
                     <div class="form-floating mt-4">
-                        <textarea name="address" style="height: 140px;" class="form-control shadow"></textarea>
+                        <textarea name="address" style="height: 140px;" class="form-control shadow" required></textarea>
                         <label for="address" class="form-label">Address:</label>
+                        <small class="invalid-feedback">Please enter your address</small>
                     </div>
                     <p class="text-danger"><?php echo $addressError ?></p>
 
@@ -213,6 +226,28 @@ function test_input($data)
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script>
+        (function() {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+    </script>
 </body>
 
 </html>
